58a5f112672a24ad9eb6c6863006f1d5d6fb856e
unknown <nayabmirza2003@gmail.com>
2025-03-16 00:37:27 +0530

    API configuration

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import './App.css'
+import { ThemeProvider } from './components/context/theme-provider'
+import Layout from './components/layout'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import WeatherDashboard from './pages/weather-dashboard'
+import CityPage from './pages/city-page'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient();
+
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme='dark'>
+          <div>
+            <Layout>
+              <Routes>
+                <Route path='/' element={<WeatherDashboard />} />
+                <Route path='/city/:cityName' element={<CityPage />} />
+              </Routes>
+            </Layout>
+
+          </div>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
+
+export default App
---
+++ src/api/weather.ts
@@ -0,0 +1,26 @@
+class WeatherAPI {
+
+    private createUrl(
+        endpoint: string,
+        params: Record<string, string | number>
+    ) {
+        const searchParams = new URLSearchParams({
+            appid: API_CONFIG.API_KEY,
+            ...params,
+        })
+        return `{endpoint}? {searchParams.toString()}`
+    }
+
+
+    private fetchData<T>(url: string): Promise<T> {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Weather API Error: ${response.statusText}');
+        }
+        return response.json();
+    }
+
+    async getCurrentWeather() { }
+    async getForecast() { }
+    async reverseGeocode() { }
+}
