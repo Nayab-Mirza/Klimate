a37801cfaac68a6855f639b240f8049b2393860e
unknown <nayabmirza2003@gmail.com>
2025-04-12 22:41:09 +0530

    Minor changes in tsconfig.json

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren } from 'react'
+import type { PropsWithChildren } from 'react'
 import Header from './header'
 
 const Layout = ({ children }: PropsWithChildren) => {
