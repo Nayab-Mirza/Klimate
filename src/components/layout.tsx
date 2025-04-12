ba3802df4f49e680b1da09e38964c6408e7f50c0
unknown <nayabmirza2003@gmail.com>
2025-04-12 17:13:13 +0530

    Save changes

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,9 +8,9 @@ const Layout = ({ children }: PropsWithChildren) => {
             <main className='min-h-screen container mx-auto px-4 py-8'>
                 {children}
             </main>
-            <footer className='border-t backdrop-blur py-12 supports-[backdrop-filter]:bg-background/60'> '
+            <footer className='border-t backdrop-blur py-12 supports-[backdrop-filter]:bg-background/60'> 
                 <div className='container mx-auto px-4 text-center text-gray-600'>
-                    <p>Made with 💗 by Nayab </p>
+                    <p>Made by Nayab 🌥️ </p>
                 </div>
             </footer>
 
